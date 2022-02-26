8bfc7aae81128c45289ff3d9f2b93c5f082a25ea
binmuji <kap.binmuji@gmail.com>
2022-02-26 17:47:56 +0700

    udemy fundamental python 3:12

--- project01.py
+++ project01.py
@@ -5,3 +5,15 @@ coba terus
 
 print ("hai dunya")
 print ("tetep asyik")
+
+ingin_cepat = False
+if ingin_cepat:
+    print('jalan lurus aja ya!')
+else:
+    print('Jalan lain')
+
+
+jumlah_anak = 4
+
+for index_anak in range(1, jumlah_anak+1): #jumlah perulangan = 5 - 1 = 4
+    print(f'Halo anak #{index_anak}')
