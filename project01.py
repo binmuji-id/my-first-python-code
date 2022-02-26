a8c044b8b89415534bf119bbf66aae71d98e083d
binmuji <kap.binmuji@gmail.com>
2022-02-26 18:15:25 +0700

    quiz 1 fundamental python

--- project01.py
+++ project01.py
@@ -17,3 +17,13 @@ jumlah_anak = 4
 
 for index_anak in range(1, jumlah_anak+1): #jumlah perulangan = 5 - 1 = 4
     print(f'Halo anak #{index_anak}')
+
+
+susu = 1
+telur = 6
+if susu == 1 and telur == 6:
+    print ('budi akhirnya membeli susu :', susu, 'dan telur :', telur)
+elif susu == 1 and telur < 6:
+    print ('budi akhirnya membeli susu :', susu, ' saja')
+else :
+    print ('budi pulang tanpa belanja apapun')
